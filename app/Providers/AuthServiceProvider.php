7a246b8ccdc4016b9f6a47bd6e996bca6e30b391
Darkbelg <stijn.sagaert@outlook.com>
2024-02-23 21:28:13 +0100

    Add policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\TimeEntry;
+use App\Policies\TimeEntryPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        TimeEntry::class => TimeEntryPolicy::class
     ];
 
     /**
