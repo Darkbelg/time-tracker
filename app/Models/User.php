804fcb328f8c86081abb3ac8c402ff58aac20768
Darkbelg <stijn.sagaert@outlook.com>
2024-02-23 19:35:50 +0100

    fix user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,30 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $fillable = [
+        'name',
+        'email',
+        'password',
         'company',
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attributes that should be hidden for serialization.
      *
-     * @var array
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
      */
     protected $casts = [
         'id' => 'integer',
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 
     public function timeEntries(): HasMany
