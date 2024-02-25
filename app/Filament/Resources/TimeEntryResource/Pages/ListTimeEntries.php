f5f7d71f89ad3fa3a1ce5bf44fd969957792191b
Darkbelg <stijn.sagaert@outlook.com>
2024-02-25 19:16:26 +0100

    Add panel filters for time entries

--- app/Filament/Resources/TimeEntryResource/Pages/ListTimeEntries.php
+++ app/Filament/Resources/TimeEntryResource/Pages/ListTimeEntries.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\TimeEntryResource\Pages;
 
-use App\Filament\Resources\TimeEntryResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\TimeEntryResource;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTimeEntries extends ListRecords
 {
@@ -14,6 +16,21 @@ class ListTimeEntries extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make(),
+            'This Week' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date', '>=', now()->startOfWeek())),
+            'Last Week' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date', '<', now()->startOfWeek())->where('date', '>=',  now()->subWeek(1)->startOfWeek())),
+            'This Month' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date', '>=', now()->firstOfMonth())),
+            'Last Month' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date', '<', now()->firstOfMonth())->where('date', '>=', now()->subMonths(1)->firstOfMonth())),
         ];
     }
 }
