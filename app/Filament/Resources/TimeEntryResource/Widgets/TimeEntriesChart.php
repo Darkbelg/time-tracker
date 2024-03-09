3f7b36a34473aa51fdfc422d5f609175511ce309
Darkbelg <stijn.sagaert@outlook.com>
2024-03-09 14:45:54 +0100

    add day filters

--- app/Filament/Resources/TimeEntryResource/Widgets/TimeEntriesChart.php
+++ app/Filament/Resources/TimeEntryResource/Widgets/TimeEntriesChart.php
@@ -11,17 +11,52 @@ class TimeEntriesChart extends ChartWidget
 {
     protected static ?string $heading = 'Chart';
 
+    public ?string $filter = 'this_week';
+
+    protected function getFilters(): ?array
+    {
+        return [
+            'this_week' => 'This week',
+            'last_week' => 'Last week',
+            'this_month' => 'This month',
+            'last_month' => 'Last month',
+        ];
+    }
+
     protected function getData(): array
     {
-        $data = TimeEntry::select(DB::raw('SUM(time) as total_time'), 'date')
-            ->where('date', '>=', now()->startOfWeek())
-            ->groupBy('date')
+        $dataQuery = TimeEntry::query()
+            ->select(DB::raw('SUM(time) as total_time'), 'date');
+
+        $datesQuery = TimeEntry::query()
+            ->select('date');
+
+
+        if ($this->filter === 'this_week') {
+            $dataQuery->where('date', '>=', now()->startOfWeek());
+            $datesQuery->where('date', '>=', now()->startOfWeek());
+        }
+
+        if ($this->filter === 'last_week') {
+            $dataQuery->where('date', '<', now()->startOfWeek())->where('date', '>=',  now()->subWeek(1)->startOfWeek());
+            $datesQuery->where('date', '<', now()->startOfWeek())->where('date', '>=',  now()->subWeek(1)->startOfWeek());
+        }
+
+        if ($this->filter === 'this_month') {
+            $dataQuery->where('date', '>=', now()->firstOfMonth());
+            $datesQuery->where('date', '>=', now()->firstOfMonth());
+        }
+
+        if ($this->filter === 'last_month') {
+            $dataQuery->where('date', '<', now()->firstOfMonth())->where('date', '>=', now()->subMonths(1)->firstOfMonth());
+            $datesQuery->where('date', '<', now()->firstOfMonth())->where('date', '>=', now()->subMonths(1)->firstOfMonth());
+        }
+
+        $data = $dataQuery->groupBy('date')
             ->orderBy('date', 'desc')
             ->pluck('total_time');
 
-        $dates = TimeEntry::select('date')
-            ->where('date', '>=', now()->startOfWeek())
-            ->groupBy('date')
+        $dates = $datesQuery->groupBy('date')
             ->orderBy('date', 'desc')
             ->pluck('date')
             ->map(function ($date) {
